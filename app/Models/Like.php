00f7abbbb875ecf56e8e2aae9ba161a304bf7606
zenithz0 <81942373+notzenotz@users.noreply.github.com>
2025-12-11 11:16:23 +0700

    Refactor Like model to include relationships

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -3,55 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
-
-class Like extends Model
-{
-=======
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Like extends Model
 {
+    protected $table = 'likes';
+
+   
     public $timestamps = false;
 
->>>>>>> origin/main
     protected $fillable = [
         'user_id',
         'recipe_id',
     ];
 
-<<<<<<< HEAD
-    public $timestamps = false; 
-    // Kalau tabel kamu punya created_at saja tanpa updated_at
-    // atau kamu handle timestamps manual.
-    
-    protected $table = 'likes';
-
-    public function user()
-=======
-    protected $casts = [
-        'created_at' => 'datetime',
-    ];
-
-    const CREATED_AT = 'created_at';
-    const UPDATED_AT = null;
-
     /**
      * Like belongs to a user
      */
     public function user(): BelongsTo
->>>>>>> origin/main
     {
         return $this->belongsTo(User::class);
     }
 
-<<<<<<< HEAD
-    public function recipe()
-    {
-        return $this->belongsTo(Recipe::class);
-    }
-}
-=======
     /**
      * Like belongs to a recipe
      */
@@ -60,4 +33,3 @@ class Like extends Model
         return $this->belongsTo(Recipe::class);
     }
 }
->>>>>>> origin/main
